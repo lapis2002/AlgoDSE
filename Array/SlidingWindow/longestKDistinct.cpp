b6861774d442d6651291a71a5e9914816eef4d0d
lapis2002 <irene.duong799@gmail.com>
2022-04-02 18:22:34 -0700

    Longest Substring with At Most K Distinct Characters - fixed cpp ver

--- Array/SlidingWindow/longestKDistinct.cpp
+++ Array/SlidingWindow/longestKDistinct.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> freq(26, 0);
+        unordered_map<char, int> freq;
         int left = 0; int right = 0; int cnt = 0;
         int n = s.length();
         int res = 0;
         
-        int sub_length;
         for (; right < n; ++ right) {
-            if (freq[s[right]-'A'] == 0) cnt ++;
-            freq[s[right]-'A'] ++;
+            if (freq.find(s[right]) == freq.end() ||
+                freq[s[right]] == 0) cnt ++;
+            if (freq.find(s[right]) != freq.end()) freq[s[right]] ++;
+            else freq.insert({s[right], 1});
 
-            if (cnt <= k) res = max(res, right - left + 1);
+            if (cnt <= k) {
+                res = max(res, right - left + 1);
+            }
             else 
                 while (cnt > k) {
-                    freq[s[left]-'A'] --;
-                    if (freq[s[left]-'A'] <= 0) cnt --;
+                    freq[s[left]] --;
+                    if (freq[s[left]] <= 0) cnt --;
                     left ++;
                 }
         }
